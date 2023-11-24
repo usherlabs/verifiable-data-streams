d13f33b77de0dfcf2942f550615b52ac3cf9dc2b
Raffael Campos <raffael.rcampos@gmail.com>
2023-11-24 10:41:31 -0300

    Update API calls and add new tests for ankr and llama networks
    The code was modified to update the way some API calls are made in the tests, improving the accuracy of the results. In addition, new tests have been introduced for the ankr and llama networks, ensuring that these networks work as expected across all network types and result in the correct gas station outputs.

--- sources/gas-station/test/index.test.ts
+++ sources/gas-station/test/index.test.ts
@@ -1,28 +1,30 @@
 import { describe, expect, test } from "vitest";
 import { blockNativeNetworkGasStations } from "../src/utils/block-native";
-import { firstValueFrom } from "rxjs";
+import { catchError, firstValueFrom, merge, tap } from "rxjs";
 import { alchemyNetworkGasStations } from "../src/utils/alchemy";
 import { BigNumber } from "alchemy-sdk";
+import { ankrNetworkGasStations } from "../src/utils/ankr";
+import { llamaNetworkGasStations } from "../src/utils/llamarpc";
 
 describe("block native", () => {
   const apiKey = process.env.BLOCK_NATIVE_KEY!;
   test("check works", async () => {
     const res = await firstValueFrom(
-      blockNativeNetworkGasStations.check(apiKey),
+      blockNativeNetworkGasStations(apiKey).check,
     );
     expect(res).toBe(true);
   });
 
   test("invalid key returns false check", async () => {
     const res = await firstValueFrom(
-      blockNativeNetworkGasStations.check("invalid"),
+      blockNativeNetworkGasStations("invalid").check,
     );
     expect(res).toBe("Blocknative API key is invalid");
   });
 
   test("polygon works", async () => {
     const res = await firstValueFrom(
-      blockNativeNetworkGasStations.polygon(apiKey),
+      blockNativeNetworkGasStations(apiKey).polygon,
     );
     expect(res.source).toBe("blocknative");
     expect(res).toMatchObject({
@@ -35,7 +37,7 @@ describe("block native", () => {
 
   test("ethereum works", async () => {
     const res = await firstValueFrom(
-      blockNativeNetworkGasStations.ethereum(apiKey),
+      blockNativeNetworkGasStations(apiKey).ethereum,
     );
     expect(res.source).toBe("blocknative");
     expect(res).toMatchObject({
@@ -60,9 +62,8 @@ describe("alchemy", () => {
     source: "alchemy",
   };
 
-
   test("check works", async () => {
-    const res = await firstValueFrom(alchemyNetworkGasStations.check(apiKey));
+    const res = await firstValueFrom(alchemyNetworkGasStations(apiKey).check);
     expect(res).toBe(true);
   });
 
@@ -75,15 +76,151 @@ describe("alchemy", () => {
 
   test("ethereum works", async () => {
     const res = await firstValueFrom(
-      alchemyNetworkGasStations.ethereum(apiKey),
+      alchemyNetworkGasStations(apiKey).ethereum,
     );
     expect(res.source).toBe("alchemy");
     expect(res).toMatchObject(expectedAlchemyShape);
   });
 
   test("polygon works", async () => {
-    const res = await firstValueFrom(alchemyNetworkGasStations.polygon(apiKey));
+    const res = await firstValueFrom(alchemyNetworkGasStations(apiKey).polygon);
     expect(res.source).toBe("alchemy");
     expect(res).toEqual(expectedAlchemyShape);
   });
+});
+
+describe(
+  "ankr",
+  () => {
+    test("single network work", async () => {
+      const res = await firstValueFrom(ankrNetworkGasStations.polygon);
+
+      expect(res).toMatchObject({
+        source: "ankr",
+        blockNumber: expect.any(Number),
+        estimatedBaseFee: expect.any(Number),
+        fast: {
+          maxFee: expect.any(Number),
+          maxPriorityFee: expect.any(Number),
+        },
+        safeLow: {
+          maxFee: expect.any(Number),
+          maxPriorityFee: expect.any(Number),
+        },
+        standard: {
+          maxFee: expect.any(Number),
+          maxPriorityFee: expect.any(Number),
+        },
+        rapid: {
+          maxFee: expect.any(Number),
+          maxPriorityFee: expect.any(Number),
+        },
+      });
+    });
+    test("All networks work", async () => {
+      const { check, ...allFns } = ankrNetworkGasStations;
+      const results$ = Object.entries(allFns).map(([network, src]) => {
+        return src.pipe(
+          tap((res) => console.log(`Got response for ${network}`, res)),
+          catchError((e) => {
+            console.error(`Error on ${network}`, e);
+            return [];
+          }),
+        );
+      });
+      const fnExecution$ = merge(...results$);
+      for await (const res of fnExecution$) {
+        console.log("Got response from ankr", res);
+        expect(res).toMatchObject({
+          source: "ankr",
+          blockNumber: expect.any(Number),
+          estimatedBaseFee: expect.any(Number),
+          fast: {
+            maxFee: expect.any(Number),
+            maxPriorityFee: expect.any(Number),
+          },
+          safeLow: {
+            maxFee: expect.any(Number),
+            maxPriorityFee: expect.any(Number),
+          },
+          standard: {
+            maxFee: expect.any(Number),
+            maxPriorityFee: expect.any(Number),
+          },
+          rapid: {
+            maxFee: expect.any(Number),
+            maxPriorityFee: expect.any(Number),
+          },
+        });
+      }
+    });
+  },
+  { timeout: 40_000 },
+);
+
+describe("llama", () => {
+  test("single network work", async () => {
+    const res = await firstValueFrom(llamaNetworkGasStations.polygon);
+    expect(res).toMatchObject({
+      source: "llama",
+      blockNumber: expect.any(Number),
+      estimatedBaseFee: expect.any(Number),
+      fast: {
+        maxFee: expect.any(Number),
+        maxPriorityFee: expect.any(Number),
+      },
+      safeLow: {
+        maxFee: expect.any(Number),
+        maxPriorityFee: expect.any(Number),
+      },
+      standard: {
+        maxFee: expect.any(Number),
+        maxPriorityFee: expect.any(Number),
+      },
+      rapid: {
+        maxFee: expect.any(Number),
+        maxPriorityFee: expect.any(Number),
+      },
+    });
+  });
+  test(
+    "All networks work",
+    async () => {
+      const { check, ...allFns } = llamaNetworkGasStations;
+      const results$ = Object.entries(allFns).map(([network, src]) => {
+        return src.pipe(
+          tap((res) => console.log(`Got response for ${network}`, res)),
+          catchError((e) => {
+            console.error(`Error on ${network}`, e);
+            return [];
+          }),
+        );
+      });
+      const fnExecution$ = merge(...results$);
+      for await (const res of fnExecution$) {
+        expect(res).toMatchObject({
+          source: "llama",
+          blockNumber: expect.any(Number),
+          estimatedBaseFee: expect.any(Number),
+          fast: {
+            maxFee: expect.any(Number),
+            maxPriorityFee: expect.any(Number),
+          },
+          safeLow: {
+            maxFee: expect.any(Number),
+            maxPriorityFee: expect.any(Number),
+          },
+          standard: {
+            maxFee: expect.any(Number),
+            maxPriorityFee: expect.any(Number),
+          },
+          rapid: {
+            maxFee: expect.any(Number),
+            maxPriorityFee: expect.any(Number),
+          },
+        });
+      }
+    },
+    { timeout: 40_000 },
+  );
 });
